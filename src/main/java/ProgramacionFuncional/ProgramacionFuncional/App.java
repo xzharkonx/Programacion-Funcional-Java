0c22c7319648f0f65f2a589d5432155084edc212
luisgarcia <luisgarcia@localhost>
2025-08-01 00:03:26 -0600

    Primer Commit

---
+++ src/main/java/ProgramacionFuncional/ProgramacionFuncional/App.java
@@ -0,0 +1,13 @@
+package ProgramacionFuncional.ProgramacionFuncional;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
